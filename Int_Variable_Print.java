0bbb3ac1a7f44fddf276676c55b48c9a28f4674e
Manashri Rahane <98026282+manashree20@users.noreply.github.com>
2024-02-09 18:32:05 +0530

    Update Int_Variable_Print.java

--- Int_Variable_Print.java
+++ Int_Variable_Print.java
@@ -1,8 +1,14 @@
+//Print Integer variable
 class Int_Variable_Print
 {
+ //Main method
  public static void main(String[]args)
   {
    int number = 10;
    System.out.println(number);
   }
 }
+
+/*Output: CT:Success
+          RT=10
+ */
